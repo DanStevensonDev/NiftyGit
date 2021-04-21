0bf961247431595e93a2cbbfb62fbc9195dec557
DanStevensonCO <danstevensonco@gmail.com>
2021-04-21 23:09:47 +0100

    remove getOfferByRef function

--- src/utils/backendApi.js
+++ src/utils/backendApi.js
@@ -6,13 +6,6 @@ const backendApi = axios.create({
     baseURL: REACT_APP_BACKEND_API_BASE_URL
 })
 
-export const getOffersByRef = (ref) => {
-    return backendApi.get(`api/bids?ref=${ref}`)
-        .then(({data}) => {
-            return data
-        })
-}
-
 export const getOffersByCommitter = (committerUsername) => {
     return backendApi.get(`/api/bids?committer=${committerUsername}`)
         .then((data) => {
@@ -20,7 +13,7 @@ export const getOffersByCommitter = (committerUsername) => {
     })
 }
 
-export const postOffer = (bidData) => {
+export const postOfferTransaction = (bidData) => {
     return backendApi.post(`api/bids`)
         .then((data) => {
             return data
