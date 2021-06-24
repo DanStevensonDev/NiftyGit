a49ddff04c8c4b715465054dc2607bcd3724e125
DanStevensonCO <danstevensonco@gmail.com>
2021-06-24 11:06:27 +0100

    add classNames to header text

--- src/components/HeaderIntro.jsx
+++ src/components/HeaderIntro.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const HeaderIntro = () => {
     return (
         <div>
-            <h1>NiftyGit</h1>
-            <p>Welcome to NiftyGit</p>
-            <p>Support open source software developers by buying commits as NFTs.</p>
+            <h1 className="title">NiftyGit</h1>
+            <div className="intro-text">
+                <h2>Welcome to NiftyGit</h2>
+                <h2>Support open source software developers by buying commits as NFTs.</h2>
+            </div>
         </div>
     );
 };
