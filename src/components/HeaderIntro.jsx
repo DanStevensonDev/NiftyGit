48658ae7f2742dc84a07db492491496fe6683a74
DanStevensonCO <danstevensonco@gmail.com>
2021-06-24 10:01:04 +0100

    remove nav

--- src/components/HeaderIntro.jsx
+++ src/components/HeaderIntro.jsx
@@ -4,11 +4,6 @@ const HeaderIntro = () => {
     return (
         <div>
             <h1>NiftyGit</h1>
-            <ul>
-                <li>How it works</li>
-                <li>Make an offer</li>
-                <li>Mint your commits</li>
-            </ul>
             <p>Welcome to NiftyGit</p>
             <p>Support open source software developers by buying commits as NFTs.</p>
         </div>
