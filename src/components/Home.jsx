a5e16de90b421f8756ce187ac4b8004063c9d5d2
DanStevensonCO <danstevensonco@gmail.com>
2021-07-06 14:36:11 +0100

    add classNames

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,16 @@ import HowItWorksInfo from './HowItWorksInfo'
 class Home extends Component {
     render() {
         return (
-            <div>
-                <header>
-                    <HeaderIntro />
-                </header>
-                <main>
-                    <MetaMaskAccountInfo />
-                    <HowItWorksInfo />
-                </main>
+            <div className="homepage-container">
+                <div class="homepage-header">
+                    <header>
+                        <HeaderIntro />
+                    </header>
+                    <main>
+                        <MetaMaskAccountInfo />
+                    </main>
+                </div>
+                <HowItWorksInfo />
             </div>
         );
     }
