73fce1711d8c30b0045e2b65fac1bda7d360e471
DanStevensonCO <danstevensonco@gmail.com>
2021-07-15 08:59:41 +0100

    add beta version banner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,10 @@ class Home extends Component {
     render() {
         return (
             <div className="homepage-container">
+                <div className="beta-version-banner">
+                    <p><strong>BETA:</strong> NiftyGit is currently in beta mode on the Rinkeby Ethereum testnet.</p>
+                    <p>Any minted commits are for test purposes only and should not be purchased with "real" Ether.</p>
+                </div>
                 <div className="homepage-header">
                     <header>
                         <HeaderIntro />
