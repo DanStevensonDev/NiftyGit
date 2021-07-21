14f36a75a430de8c0a920597f9a878808f70b213
DanStevensonCO <danstevensonco@gmail.com>
2021-07-21 10:06:54 +0100

    remove line from header text

--- src/components/HeaderIntro.jsx
+++ src/components/HeaderIntro.jsx
@@ -5,7 +5,6 @@ const HeaderIntro = () => {
         <div>
             <h1 className="title">NiftyGit</h1>
             <div className="intro-text">
-                <h2>Welcome to NiftyGit</h2>
                 <h2>Support open source software developers by buying commits as NFTs.</h2>
             </div>
         </div>
