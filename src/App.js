43a07c87ce2728c018a8231a883f441505ae1c2d
DanStevensonCO <danstevensonco@gmail.com>
2021-07-21 11:00:04 +0100

    add event listeners to check when user changes their account or chainId from within their crypto wallet

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Home from './components/Home'
 import MintMyCommits from './components/MintMyCommits'
 
 function App() {
+  // add event listeners at top level of App
+  // to check whenever crypto account or chainId is changed
+  // and reload the page
+  window.ethereum.on('accountsChanged', () => {
+    window.location.reload();
+  })
+
+  window.ethereum.on('chainChanged', () => {
+    window.location.reload();
+  });
+
   return (
     <div className="App">
       <Router className="content">
