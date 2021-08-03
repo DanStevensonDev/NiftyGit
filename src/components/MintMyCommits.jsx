7c392e8073eadeb160bdff0e0c55423a2b4daef0
DanStevensonCO <danstevensonco@gmail.com>
2021-08-03 21:36:30 +0100

    add MintMyCommits table styling and MaterialUI button

--- src/components/MintMyCommits.jsx
+++ src/components/MintMyCommits.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import socialMediaAuth from '../service/auth';
 
 import { getOffersByCommitter } from '../utils/backendApi'
+
+import { Button } from "@material-ui/core"
 class MintMyCommits extends Component {
     state = {
         isGithubAuthenticated: false,
@@ -36,7 +38,7 @@ class MintMyCommits extends Component {
         if (!this.state.isGithubAuthenticated) {
             return (
                 <div>
-                    <button onClick={() => this.handleGithubLogin()}>Login to Github</button>
+                    <Button variant="contained" onClick={() => this.handleGithubLogin()}>Login to Github</Button>
                 </div>
                 
             )
@@ -50,24 +52,27 @@ class MintMyCommits extends Component {
                     <p>Logged in</p>
                     <p>Hi {committerUsername}</p>
                     <p>Email address: {committerEmailAddress}</p>
-                    <table>
+                    <table id="commit-offers">
                         <thead>
                           <tr>
-                            <th>Commit URL</th>
+                            <th>Repo</th>
+                            <th>Commit</th>
                             <th>Offer amount</th>
                             <th>Mint commit</th>
                             </tr>
                         </thead>
                         <tbody>
                             {offersData.map((offer) => {
-                                // console.log(offer)
+                                console.log(offer)
                                 const { offerId, supporterAccountAddress, offerAmountInEth, commitData } = offer
+                                const repo = commitData.html_url.split("https://github.com/")[1].split("/commit")[0]
                                 const commitSHA = commitData.sha
                                 return (
                                     <tr key={offerId}>
+                                        <td>{repo}</td>
                                         <td><a href={commitData.html_url} target="_blank" rel="noreferrer">{commitData.commit.message}</a></td>
-                                        <td>{offerAmountInEth}eth</td>
-                                        <td><button onClick={() => this.handleMintCommit(commitSHA, supporterAccountAddress, offerAmountInEth)}>Accept offer and mint</button></td>
+                                        <td>{offerAmountInEth} eth</td>
+                                        <td><Button variant="contained" onClick={() => this.handleMintCommit(commitSHA, supporterAccountAddress, offerAmountInEth)}>Accept offer and mint</Button></td>
                                     </tr>
                                 )
                         })}
